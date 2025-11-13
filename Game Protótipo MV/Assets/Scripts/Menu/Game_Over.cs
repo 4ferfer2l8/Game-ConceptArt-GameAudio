21837b6ac9f19f9935d22bbeeb50db34886b8009
galinaco <167914498+galinaco@users.noreply.github.com>
2025-11-13 13:17:18 -0300

    menus e fontes

--- Game Protótipo MV/Assets/Scripts/Menu/Game_Over.cs
+++ Game Protótipo MV/Assets/Scripts/Menu/Game_Over.cs
@@ -32,6 +32,6 @@ public class Game_Over : MonoBehaviour
 
     public void ReiniciaBoss()
     {
-               SceneManager.LoadScene("FaseBoss");
+        SceneManager.LoadScene("FaseBoss");
     }
 }
