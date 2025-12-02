20919f2204d118aa3b236466de377825934ad7d7
galinaco <167914498+galinaco@users.noreply.github.com>
2025-12-02 12:18:45 -0300

    contadordezumbis

---
+++ Game Protótipo MV/Assets/Scripts/ContaZumbis.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ContaZumbis : MonoBehaviour
+{
+    int contadorZumbis = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        contadorZumbis = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contadorZumbis = GameObject.FindGameObjectsWithTag("Zumbi").Length;
+    }
+}
