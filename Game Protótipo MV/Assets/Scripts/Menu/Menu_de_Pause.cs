07fb66909faf6bb349ddb01d592fa5e527444e8e
galinaco <167914498+galinaco@users.noreply.github.com>
2025-12-02 19:32:59 -0300

    menu pause

---
+++ Game Protótipo MV/Assets/Scripts/Menu/Menu_de_Pause.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Menu_de_Pause : MonoBehaviour
+{
+    [SerializeField] GameObject menudepause;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.Escape))
+       {
+            Time.timeScale = 0;
+        }
+    }
+    
+   public void BVoltar()
+    {
+        Time.timeScale = 1;
+        menudepause.SetActive(false);
+    }
+}
